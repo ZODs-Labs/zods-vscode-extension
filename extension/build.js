a5b05bf6648137b6de723b2384d1679a2441279e
Sol <sol.engineer020@gmail.com>
2025-05-13 17:49:41 +0200

    v1.0.3

--- extension/build.js
+++ extension/build.js
@@ -10,13 +10,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const define = isProduction
    ? {
-        EXTENSION_VERSION: '"1.0.2"',
+        EXTENSION_VERSION: '"1.0.3"',
         API_URL: '"https://app.zods.io/api"',
         WEB_URL: '"https://app.zods.io"',
         LOGIN_WEB_URI: '"https://app.zods.io/auth/vscode?redirectUrl="',
      }
    : {
-        EXTENSION_VERSION: '"1.0.2"',
+        EXTENSION_VERSION: '"1.0.3"',
         API_URL: '"http://localhost:5002/api"',
         WEB_URL: '"http://localhost:3000"',
         LOGIN_WEB_URI: '"http://localhost:3000/auth/vscode?redirectUrl="',
